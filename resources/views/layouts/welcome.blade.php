5432f472946023375189b21098e8ad439e0bb4b0
EdyFreela <31071891+EdyFreela@users.noreply.github.com>
2017-10-06 16:38:10 -0300

    Landing Page - Design Layout
    Bootstrap Jquery Form Validation

---
+++ resources/views/layouts/welcome.blade.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Eterna Memória</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+
+    <style>
+        /* GERAL */
+        body {
+            font-family: 'Quicksand', sans-serif;
+        }
+        .btn{
+            color:#000;
+            font-weight: bold;
+        }
+        .fa-btn {
+            margin-right: 6px;
+        }
+
+        /* NAVBAR */
+        .navbar {
+            border: 0px solid transparent;
+        }
+        nav.navbar{
+           -webkit-transition: all 0.8s ease;
+           transition: all 0.8s ease;
+        }
+        nav.navbar{
+            min-height: 160px;
+            padding-top: 20px;
+            background-color: transparent;    
+        }
+        .navbar-default .navbar-nav>li>a {
+            color: #fff;
+           -webkit-transition: all 0.3s ease;
+           transition: all 0.3s ease;            
+            padding-top: 33px;
+            padding-bottom: 15px;
+        }       
+        .navbar-default .navbar-nav>li>a:focus, 
+        .navbar-default .navbar-nav>li>a:hover {
+            color: orange;
+        }                
+        nav.navbar.shrink {
+            min-height: 85px;
+            padding-top: 0px;
+            background-color: #232525;
+            color:#000;
+        }       
+        .shrink-img {
+            -webkit-transition: -webkit-transform 0.3s;
+            transition: transform 0.3s;
+            -webkit-transform: scale(0.7);
+            transform: scale(0.7);
+        }
+        .shrink-navbar{
+            padding: 3px 15px;
+        }
+
+        /* CONTENT */                
+        .section-one{
+            min-height: 100vh;
+            background-color: #000;
+            background: url('{{asset('assets/img/banner-eternizando-momentos.jpg')}}') no-repeat center center fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;            
+        }
+        .section-two{
+            min-height: calc(100vh - 85px);
+            padding-top:30px;
+            background-color: #f1f1f1;
+        }
+        .section-two .panel-produto{
+            border: 0;
+            background: none;
+            box-shadow: none;
+        }
+        .section-two .panel-produto .quantidade strong{
+            font-size: 10px;
+        }
+        .section-two .panel-produto .quantidade input{
+            font-weight:bold;
+        }
+        .cep-error {
+            margin-top: 25px;
+            font-weight: bold;
+            color: red;
+        }
+        .section-tree{
+            min-height: calc(100vh - 85px);
+            background-color: #fff;
+            background: url('{{asset('assets/img/banner-como-funciona.jpg')}}') no-repeat center center;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover; 
+            font-size:20px;
+        }
+        .section-tree h1{
+            margin-top: 30px;
+            margin-bottom: 30px;
+        }
+        .section-tree p{
+            margin-bottom: 30px;
+        }        
+        .section-tree .frase{
+            margin-top:70px;
+        }
+        .section-tree .frase i {
+            color:orange;
+        }
+        .section-footer{
+            background-color: #000;
+            color: #fff;
+            padding-top: 40px;
+            padding-bottom: 40px;
+            font-size: 10px;
+        }
+        .section-footer h1{
+            margin-top: 0;
+            margin-bottom: 10px;
+            font-size: 14px;
+        }        
+        .section-footer .links ul{margin:0;padding:0;}
+        .section-footer .links li{list-style: none; margin-bottom:10px;}
+        .section-footer .btn-facebook{
+            border-radius:100%;
+            height: 40px;
+            width: 40px;
+            font-size: 18px;
+            color: #000;
+        }
+        .section-footer .btn-instagram{
+            border-radius:100%;
+            height: 40px;
+            width: 40px;                        
+            font-size: 18px;
+            color: #000;            
+        }
+        .section-footer a{
+            color:#fff;
+        }
+        .section-footer .links a{
+            color:#fff;
+        }
+        .section-footer .links ul{
+            display: block !important;
+        }
+        .section-footer .links ul li{
+            list-style: none; 
+            margin-bottom:10px;
+        }        
+
+        /* Extra Small Devices, Phones */ 
+        @media only screen and (min-width : 320px) {
+            .section-one{
+                background: url('{{asset('assets/img/banner-eternizando-momentos.jpg')}}') no-repeat center center fixed;
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+                background-size: cover;            
+            }            
+            .section-two .btn-finalizar-compra{
+                margin-top: 20px;
+                margin-bottom: 20px;
+            }
+            .section-tree .frase{
+                text-align: center;
+            }
+            .section-footer .links ul{
+                display: inline-flex;
+            }
+            .section-footer .links ul li{
+                text-align: center !important;
+            }            
+        }         
+    </style>
+    @yield('style')
+</head>
+<body id="app-layout">
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <img src="{{asset('/assets/img/logo-eterna-memoria.png')}}" id="brand-img">
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    @if (Auth::guest())
+                    @else
+                        <li><a href="{{ url('/home') }}">Home</a></li>
+                    @endif
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ url('/register') }}">Primeiro Acesso</a></li>
+                        <li><a href="{{ url('/login') }}">Login</a></li>
+                        <li><a href="{{ url('/#comprar') }}">Comprar</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    @yield('content')
+
+    <div class="section-footer">
+        <div class="container">
+            <div class="col-md-2 links">
+                <ul>
+                    <li class="col-xs-4 col-sm-12 col-md-12 col-lg-12"><h1><a href="{{ url('/register')}}">PRIMEIRO ACESSO</a></h1></li>
+                    <li class="col-xs-4 col-sm-12 col-md-12 col-lg-12"><h1><a href="{{ url('/login')}}">LOGIN</a></h1></li>
+                    <li class="col-xs-4 col-sm-12 col-md-12 col-lg-12"><h1><a href="#comprar">COMPRAR</a></h1></li>
+                </ul>
+            </div>
+            <div class="col-md-8 text-center endereco">
+                <h1>Revenda Produtos Eterna Memória</h1>
+                <p>Teremos prazer de entrar em contato com você.</p>
+                {!! Form::open(array('route' => 'pedido.store','method'=>'POST')) !!}
+                <div class="col-xs-12 col-sm-12 col-md-6 col-md-offset-3">
+                    <div class="form-group">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Digite seu E-mail">
+                            <span class="input-group-btn">
+                                <button class="btn btn-default btn-warning" type="button">ENVIAR</button>
+                            </span>
+                        </div>                    
+                    </div>
+                </div>
+                {!! Form::close() !!}                
+            </div>
+            <div class="col-md-2 text-center social">
+                <h1>Contato</h1>
+                <p><a href="mailto:eternamemoria@gmail.com">eternamemoria@gmail.com</a></p>
+                <a href="" class="btn btn-default btn-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                <a href="" class="btn btn-default btn-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+            </div>
+        </div>
+    </div>
+
+    <!-- JavaScripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+    <script>
+        // Smooth Scroller
+        $('a[href*="#"]')
+          // Remove links that don't actually link to anything
+          .not('[href="#"]')
+          .not('[href="#0"]')
+          .click(function(event) {
+            // On-page links
+            if (
+              location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+              && 
+              location.hostname == this.hostname
+            ) {
+              // Figure out element to scroll to
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+              // Does a scroll target exist?
+              if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                  scrollTop: target.offset().top
+                }, 1000, function() {
+                  // Callback after animation
+                  // Must change focus!
+                  var $target = $(target);
+                  $target.focus();
+                  if ($target.is(":focus")) { // Checking if the target was focused
+                    return false;
+                  } else {
+                    $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                    $target.focus(); // Set focus again
+                  };
+                });
+              }
+            }
+          }); 
+    </script>
+    @yield('script')
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,321 @@
-@extends('layouts.app')
+@extends('layouts.welcome')
+
+@section('style')
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Bem Vindo</div>
-
-                <div class="panel-body">
-                    Landing Page.
+<div class="section-one">
+    <div class="container"></div>
+</div>
+<div class="section-two">
+    <a id="comprar"></a>
+    <div class="container">
+        <div class="col-md-12">
+            <h2>FAÇA SEU PEDIDO AGORA</h2>
+        </div>
+        {!! Form::open(array('route' => 'pedido.store','method'=>'POST', 'id'=>'formPedido')) !!}
+        <div class="col-md-12">
+            <div class="panel panel-default panel-produto">
+              <div class="panel-body">
+                <div class="col-md-12">
+                    <div class="col-xs-12 col-sm-3 col-md-1 text-center">
+                        <div class="form-group quantidade">
+                            <strong>Quantidade</strong>
+                            <div class="validator">
+                            {!! Form::text('quantidade', '1', array('class' => 'form-control quantidade text-center')) !!}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-3 col-md-3 text-center">
+                        <h3 class="total">Tag: R$ 39,99</h3>
+                    </div>                                        
+                    <div class="col-xs-12 col-sm-3 col-md-1 text-center">
+                        <h3>+</h3>
+                    </div>
+                    <div class="col-xs-12 col-sm-3 col-md-3 text-center">
+                        <h3>Frete: R$ 5,50</h3>
+                    </div>
+                    <div class="col-xs-12 col-sm-3 col-md-1 text-center">
+                        <h3>=</h3>
+                    </div>
+                    <div class="col-xs-12 col-sm-3 col-md-3 text-center">
+                        <h3 class="total-geral">Total: R$ 45,49</h3>
+                    </div>                                                                                       
+                </div>
+              </div>
+            </div>
+        </div>        
+        <div class="col-md-6">
+            <h2>DADOS PESSOAIS</h2>
+            <div class="col-xs-6 col-sm-6 col-md-6">
+                <div class="form-group">
+                    <strong>*Nome</strong>
+                    <div class="validator">
+                    {!! Form::text('nome', null, array('class' => 'form-control', 'id' => 'nome')) !!}
+                    </div>
+                </div>
+            </div>            
+            <div class="col-xs-6 col-sm-6 col-md-6">
+                <div class="form-group">
+                    <strong>*Sobrenome</strong>
+                    <div class="validator">
+                    {!! Form::text('sobrenome', null, array('class' => 'form-control', 'id' => 'sobrenome')) !!}
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>*Seu melhor e-mail</strong>
+                    <div class="validator">
+                    {!! Form::text('email', null, array('class' => 'form-control', 'id' => 'email')) !!}
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-sm-6 col-md-6">
+                <div class="form-group">
+                    <strong>*CPF</strong>
+                    <div class="validator">
+                    {!! Form::text('cpf', null, array('class' => 'form-control cpf')) !!}
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-sm-6 col-md-6">
+                <div class="form-group">
+                    <strong>*Celular</strong>
+                    <div class="validator">
+                    {!! Form::text('celular', null, array('class' => 'form-control celular')) !!}
+                    </div>
                 </div>
             </div>
         </div>
+        <div class="col-md-6">
+            <h2>ENDEREÇO DE ENTREGA</h2>
+            <div class="col-xs-12 col-sm-6 col-md-6">
+                <div class="form-group">
+                    <strong>*Informe seu CEP</strong>
+                    <div class="input-group">
+                        <div class="validator">
+                            <input type="text" name="cep" class="form-control cep">
+                        </div>
+                        <span class="input-group-btn">
+                            <button class="btn btn-default btn-warning" type="button" onclick="buscaCep();">Procurar</button>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-sm-6 col-md-6 cep-error"></div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>*Endereço</strong>
+                    <div class="validator">
+                    {!! Form::text('endereco', null, array('class' => 'form-control endereco')) !!}
+                    </div>
+                </div>
+            </div>             
+            <div class="col-xs-12 col-sm-12 col-md-4">
+                <div class="form-group">
+                    <strong>*Numero</strong>
+                    <div class="validator">
+                    {!! Form::text('numero', null, array('class' => 'form-control numero')) !!}
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-8">
+                <div class="form-group">
+                    <strong>*Bairro</strong>
+                    <div class="validator">
+                    {!! Form::text('bairro', null, array('class' => 'form-control bairro')) !!}
+                    </div>
+                </div>
+            </div>             
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Complemento</strong>
+                    <div class="validator">
+                    {!! Form::text('complemento', null, array('class' => 'form-control complemento')) !!}
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-2">
+                <div class="form-group">
+                    <strong>*UF</strong>
+                    <div class="validator">
+                    {!! Form::text('uf', null, array('class' => 'form-control uf')) !!}
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-10">
+                <div class="form-group">
+                    <strong>*Cidade</strong>
+                    <div class="validator">
+                    {!! Form::text('cidade', null, array('class' => 'form-control cidade')) !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="col-xs-12 col-sm-12 col-md-12 text-left">
+                Ao clickar no botão <strong>"finalizar compra"</strong> você será redirecionado para o UOL PagSeguro para finalizar a compra.
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button class="btn btn-warning btn-finalizar-compra" style="width:100%;">Finalizar Compra</button>
+            </div>
+        </div>                                            
+        {!! Form::close() !!}
     </div>
 </div>
+<div class="section-tree">
+    <div class="container">
+        <h1 class="text-center">COMO FUNCIONA?</h1>
+        <p>Semelhante a uma página do facebook, o site <strong>Eterna Memória&copy;</strong> contém informações que você e amigos adicionam sobre seu ente querido, tais como: uma biográfia e histórias, fotos, comentários de amigos e parentes e até links para compartilhar conteúdo em sites sociais como Facebook e Twitter.</p>
+        <p>O <strong>Eterna Memória&copy;</strong> combina memorialização tecnológica com as lápides de Granito Trádicionais, adicionando um Qr-Code a lápide de granito que permite aos usuários de telefones inteligentes se conectem ao seu site personalizado. Pode ser visualizado no telefone á medida que os visitantes atravessam o cemitério ou no seu computador digitando o endereço web exclusivo no seu navegador de internet.</p>
+        <div class="col-md-5">
+            <div class="frase">
+                <h1><i class="fa fa-quote-left" aria-hidden="true"></i> VELHOS MOMENTOS, GRANDES SAUDADES E ETERNAS MEMÓRIAS <i class="fa fa-quote-right" aria-hidden="true"></i></h1>
+                <p>Heloisa Burlet.</p>
+            </div>
+        </div>
+        <div class="col-md-6"></div>
+    </div>
+</div>
+@endsection
+
+@section('script')
+<script>
+    $(window).scroll(function() {
+      if ($(document).scrollTop() > 50) {
+        $('nav').addClass('shrink');
+        $('#brand-img').addClass('shrink-img');
+        $('.navbar-brand').addClass('shrink-navbar');
+      } else {
+        $('nav').removeClass('shrink');
+        $('#brand-img').removeClass('shrink-img');
+        $('.navbar-brand').removeClass('shrink-navbar');
+      }
+    });    
+</script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.0/jquery.mask.js"></script>
+<script>
+$(document).ready(function () {
+    $(".cpf").mask('000.000.000-00', {reverse: true});
+    $(".celular").mask('(00) 0 0000.0000', {reverse: true});
+    $(".cep").mask('00 000-000', {reverse: true});
+});    
+</script>
+<script>
+    $(".quantidade").blur(function() {
+        var qtd = $(this).val();
+        var total = 39.99 * qtd;
+        $( ".total" ).html('Tag: R$ ' + total);        
+        var totalGeral = 39.99 * qtd + 5.50;
+        $( ".total-geral" ).html('Total: R$ ' + totalGeral);
+    });
+</script>
+<script>
+    function limpa_formulário_cep() {
+        // Limpa valores do formulário de cep.
+        $(".endereco").val("");
+        $(".bairro").val("");
+        $(".uf").val("");
+        $(".cidade").val("");
+    }    
+    function buscaCep(){
+        var cep = $(".cep").val().replace(/\D/g, '');
+
+        if (cep != "") {
+
+            var validacep = /^[0-9]{8}$/;
+
+            if(validacep.test(cep)) {
+
+                $(".endereco").val("...");
+                $(".bairro").val("...");
+                $(".uf").val("...");
+                $(".cidade").val("...");
+
+                $.getJSON("//viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
+
+                    if (!("erro" in dados)) {
+                        $(".endereco").val(dados.logradouro);
+                        $(".bairro").val(dados.bairro);
+                        $(".uf").val(dados.uf);
+                        $(".cidade").val(dados.localidade);
+                    } else {
+                        limpa_formulário_cep();
+                        $(".cep-error").html('<i class="fa fa-exclamation-triangle" aria-hidden="true"></i> CEP não encontrado.');
+                    }
+                });
+            } else {
+                limpa_formulário_cep();
+                $(".cep-error").html('<i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Formato de CEP inválido.');
+            }
+        } else {
+            limpa_formulário_cep();
+        }
+    }
+</script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+<script type="text/javascript">
+    $.validator.setDefaults( {
+        submitHandler: function () {
+            $( "#formPedido" ).submit();
+        }
+    } );
+
+    $( document ).ready( function () {
+        $( "#formPedido" ).validate( {
+            rules: {
+                quantidade: "required",
+                nome: "required",
+                sobrenome: "required",
+                email: "required",
+                cpf: "required",
+                celular: "required",
+                cep: "required",
+                endereco: "required",
+                numero: "required",
+                bairro: "required",
+                uf: "required",
+                cidade: "required"
+            },
+            messages: {
+                quantidade: "Você deve informar a quantidade",
+                nome: "Informe seu Nome",
+                sobrenome: "Informe seu Sobrenome",
+                email: "Informe seu E-Mail",
+                cpf: "Informe seu CPF",
+                celular: "Informe seu Celular",
+                cep: "Informe o CEP para entrega",
+                endereco: "Informe o Endereço para entrega",
+                numero: "Informe o Numero para entrega",
+                bairro: "Informe o Bairro para entrega",
+                uf: "Informe a UF para entrega",
+                cidade: "Informe a Cidade para entrega"
+            },
+            errorElement: "em",
+            errorPlacement: function ( error, element ) {
+                // Add the `help-block` class to the error element
+                
+                /*
+                error.addClass( "help-block" );
+
+                if ( element.prop( "type" ) === "checkbox" ) {
+                    error.insertAfter( element.parent( "label" ) );
+                } else {
+                    error.insertAfter( element );
+                }
+                */
+            },
+            highlight: function ( element, errorClass, validClass ) {
+                $( element ).parents( ".validator" ).addClass( "has-error" ).removeClass( "has-success" );
+            },
+            unhighlight: function (element, errorClass, validClass) {
+                $( element ).parents( ".validator" ).addClass( "has-success" ).removeClass( "has-error" );
+            }
+        });
+    });
+</script>
 @endsection
